fd2eaa1f71e0213ca71bae9ae196b7239c72a8a4
ConstReznikov <73704986+ConstReznikov@users.noreply.github.com>
2021-05-22 15:40:19 +0300

    imgs

--- Test/script.js
+++ Test/script.js
@@ -19,26 +19,40 @@ const btnNext = document.getElementById("btn-next");
 let score = 0;
 
 const correctAnswer = document.getElementById("correct-answer");
+const myOpinion = document.getElementById("my-opinion");
 const numberOfAllQuestions2 = document.getElementById(
   "number-of-all-questions-2"
 );
 const btnTryAgain = document.getElementById("btn-try-again");
 
 const questions = [
-  { question: "2+2=?", options: ["4", "3", "2", "1"], rightAnswer: 0 },
+  { question: "Who is this", options: ["zook", "makima", "ayaka", "Hiro"], rightAnswer: 0, img:true, imgsrc:"img/1.jpg" },
+  { question: "Who is this", options: ["zook", "makima", "ayaka", "Hiro"], rightAnswer: 1, img:true, imgsrc:"img/2.png" },
+  { question: "What is Klee's algorithm", options: ["Jumpy Dumpies", "Given starting and ending positions of segments on a line, the task is to take the union of all given segments and find length covered by these segments.", "I dont know", "Help me"], rightAnswer: 1, img:false  },
+  { question: "Who is this", options: ["Cuckold", "makima", "ayaka", "Hiro"], rightAnswer: 0, img:true, imgsrc:"img/3.jpg" },
+  { question: "Who is this", options: ["Zero Two", "makima", "ayaka", "Hiro"], rightAnswer: 0, img:true, imgsrc:"img/4.jpg" },
+  { question: "Who is this", options: ["Trash", "Nobody", "Slut", "Worthless shit"], rightAnswer: 0, img:true, imgsrc:"img/5.jpg" },
+  { question: "Who is this", options: ["Based Chad", "Shirou", "ayaka", "Hiro"], rightAnswer: 0, img:true, imgsrc:"img/6.jpg" },
 
-  { question: "5+2=?", options: ["4", "7", "2", "1"], rightAnswer: 1 },
-
-  { question: "10+2=?", options: ["12", "3", "2", "1"], rightAnswer: 0 },
-
-  { question: "2+222=?", options: ["4", "3", "2", "224"], rightAnswer: 3 },
 ];
-
+//
 numberOfAllQuestions.innerHTML = questions.length;
 
 const load = () => {
-  question.innerHTML = questions[indexOfQuestion].question;
 
+  if(questions[indexOfQuestion].img==true){
+    let image = document.createElement("img");
+    image.src=questions[indexOfQuestion].imgsrc;
+    image.classList.add("image");
+    question.innerHTML = questions[indexOfQuestion].question;
+
+    question.appendChild(image);
+           // image.src = "IMAGE URL/PATH"
+}
+else{
+  question.innerHTML = questions[indexOfQuestion].question;
+  
+}
   option1.innerHTML = questions[indexOfQuestion].options[0];
   option2.innerHTML = questions[indexOfQuestion].options[1];
   option3.innerHTML = questions[indexOfQuestion].options[2];
@@ -132,6 +146,27 @@ for (option of optionElements) {
 
 const quizOver = () => {
   document.querySelector(".quiz-over-modal").classList.add("active");
+
+  switch(score){
+    case 0:
+      myOpinion.innerHTML="You are trash";
+      break;
+    case 1:
+    case 2:
+    case 3:
+      myOpinion.innerHTML="Not Great not terrible";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      myOpinion.innerHTML="Almost Perfect, Almost";
+      break;
+    case 7:
+      myOpinion.innerHTML="Supre lit, based and def not cringe";
+      break;
+
+  }
+ 
   correctAnswer.innerHTML = score;
   numberOfAllQuestions2.innerHTML = questions.length;
 };
